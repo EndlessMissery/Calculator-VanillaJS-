d4e6af9c1ff5b67c808a48933089d46a49af62ce
EndlessMissery <111017439+EndlessMissery@users.noreply.github.com>
2024-02-09 00:38:01 +0100

    index.js

--- index.js
+++ index.js
@@ -1,23 +1,28 @@
+//1st number
 var n1 = document.getElementById("num1");
+
+//2nd number
 var n2 = document.getElementById("num2");
+
+//result
 var res = document.getElementById("result");
 
-//Add
+//Addition
     document.getElementById("add").addEventListener("click", function() {
       res.value = parseFloat(n1.value)+parseFloat(n2.value)
     })
 
-//Sub
+//Subtraction
     document.getElementById("sub").addEventListener("click", function() {
         res.value = parseFloat(n1.value)-parseFloat(n2.value)
     })
 
-//Mul
+//Multiplication
     document.getElementById("mul").addEventListener("click", function() {
         res.value = parseFloat(n1.value)*parseFloat(n2.value)
     })
     
-//Div
+//Division
     document.getElementById("div").addEventListener("click", function() {
         res.value = parseFloat(n1.value)/parseFloat(n2.value)
     })
